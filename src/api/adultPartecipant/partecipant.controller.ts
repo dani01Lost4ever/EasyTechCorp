9da229b3ce9fda777af620db1dd987f332284ec4
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-22 22:40:25 +0200

    Update partecipant.controller.ts

--- src/api/adultPartecipant/partecipant.controller.ts
+++ src/api/adultPartecipant/partecipant.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { TypedRequest } from "../../utils/typed-request.interface";
-import { AddPartecipantDTO, getOnePartecipantDTO } from "./partecipant.dto";
+import { AddPartecipantDTO, findPartecipantDTO, getOnePartecipantDTO, updateDTO } from "./partecipant.dto";
 import partecipantService from "./partecipant.service";
 
 export const addPartecipant = async(
@@ -17,13 +17,14 @@ export const addPartecipant = async(
     const nationalInsuranceNumber = req.body.nationalInsuranceNumber;
     const email = req.body.email;
     const telephoneNumber = req.body.telephoneNumber;
-    const tipology = req.body.tipology;
+    const tipology = req.body.tipologyId;
     const iscriptionForm = req.body.iscriptionForm;
     const privacyAccepted = req.body.privacyAccepted;
     const imageReleaseAccepted = req.body.imageReleaseAccepted;
     const paymentDone = req.body.paymentDone;
+    const paymentVerified = req.body.paymentVerified;
 
-    const partecipant = await partecipantService.add(axeraCode,firstName,lastName,age,address,nationalInsuranceNumber,email,telephoneNumber,tipology,iscriptionForm,privacyAccepted,imageReleaseAccepted,paymentDone);
+    const partecipant = await partecipantService.add(firstName,lastName,age,address,nationalInsuranceNumber,email,telephoneNumber,tipology,axeraCode,iscriptionForm,privacyAccepted,imageReleaseAccepted,paymentDone,paymentVerified);
   
   res.json(partecipant);
     }
@@ -33,12 +34,19 @@ export const addPartecipant = async(
   }
 
 export const partecipantList = async(
-    req: Request,
+    req: TypedRequest<findPartecipantDTO>,
     res: Response,
     next: NextFunction
   ) => {
     try{    
-        const partecipantList = await partecipantService.find();
+      const tipology = req.body.tipologyId;
+      const iscriptionForm = req.body.iscriptionForm;
+      const privacyAccepted = req.body.privacyAccepted;
+      const imageReleaseAccepted = req.body.imageReleaseAccepted;
+      const paymentDone = req.body.paymentDone;
+      const paymentVerified = req.body.paymentVerified;
+
+        const partecipantList = await partecipantService.find(tipology, iscriptionForm, privacyAccepted, imageReleaseAccepted, paymentDone, paymentVerified);
   
         res.json(partecipantList);
     }
@@ -62,3 +70,26 @@ export const getOnePartecipant = async(
         next(err);
     }
   }
+
+  
+export const updatePartecipant = async(
+  req: TypedRequest<updateDTO>,
+  res: Response,
+  next: NextFunction
+) => {
+  try{    
+    const partecipant = req.body.partecipantId;
+    const iscriptionForm = req.body.iscriptionForm;
+    const privacyAccepted = req.body.privacyAccepted;
+    const imageReleaseAccepted = req.body.imageReleaseAccepted;
+    const paymentDone = req.body.paymentDone;
+    const paymentVerified = req.body.paymentVerified;
+
+      const partecipantList = await partecipantService.update(partecipant,{iscriptionForm: iscriptionForm, privacyAccepted: privacyAccepted, imageReleaseAccepted: imageReleaseAccepted, paymentDone: paymentDone, paymentVerified: paymentVerified});
+
+      res.json(partecipantList);
+  }
+  catch(err){
+      next(err);
+  }
+}
