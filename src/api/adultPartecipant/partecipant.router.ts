60fd5b16e4e2e90f57a2b05eeacc14187b6ace7d
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-22 22:40:26 +0200

    Update partecipant.router.ts

--- src/api/adultPartecipant/partecipant.router.ts
+++ src/api/adultPartecipant/partecipant.router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { AddPartecipantDTO, getOnePartecipantDTO } from "./partecipant.dto";
-import { addPartecipant, getOnePartecipant, partecipantList } from "./partecipant.controller";
+import { AddPartecipantDTO, getOnePartecipantDTO, updateDTO } from "./partecipant.dto";
+import { addPartecipant, getOnePartecipant, partecipantList, updatePartecipant } from "./partecipant.controller";
 import { validate } from "../../utils/validation.middleware";
 import { isAuthenticated } from "../../utils/auth/authenticated.middleware";
 
@@ -10,5 +10,6 @@ router.use(isAuthenticated);
 router.post('/add', validate(AddPartecipantDTO), addPartecipant);
 router.get('/find', partecipantList);
 router.get('/getOne', validate(getOnePartecipantDTO), getOnePartecipant);
+router.patch('/update', validate(updateDTO), updatePartecipant);
 
 export default router;
