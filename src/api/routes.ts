6f1842ac052eade7038c1939b830e943232264be
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-22 19:44:31 +0200

    Update routes.ts

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,11 +3,13 @@ import userRouter from "./users/user.router";
 import authRouter from "./auth/auth.router";
 import partecipantRouter from "./adultPartecipant/partecipant.router";
 import tipologyRouter from "./classTipology/classTipology.router";
+import uPartecipantRouter from "./underagePartecipant/uPartecipant.router";
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/partecipant", partecipantRouter);
 router.use("/tipology", tipologyRouter);
+router.use("/upartecipant", uPartecipantRouter);
 router.use(authRouter);
 
 export default router;
