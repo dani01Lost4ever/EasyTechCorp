f275c071652983a680c040cd21b5fa941710e80b
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-22 15:34:05 +0200

    Add partecipant.model.ts

---
+++ src/api/adultPartecipant/partecipant.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+import { Partecipant as iPartecipant } from "./partecipant.entity";
+
+export const partecipantSchema = new Schema<iPartecipant>({
+    axeraCode: Boolean,
+    //dati utente
+    firstName: String,
+    lastName: String,
+    age: Number,
+    address: String,
+    nationalInsuranceNumber: String,
+    email: String, 
+    telephoneNumber: Number,
+    tipology: String,
+    //checkboxS
+    iscriptionForm: Boolean,
+    privacyAccepted: Boolean,
+    imageReleaseAccepted: Boolean,
+    paymentDone: Boolean
+})
+
+partecipantSchema.set("toJSON", {
+    virtuals: true,
+    transform: (_, ret) => {
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    },
+  });
+
+  partecipantSchema.set("toObject", {
+    virtuals: true,
+    transform: (_, ret) => {
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    },
+  });
+
+export const Partecipant = mongoose.model<iPartecipant>("Partecipants", partecipantSchema);
